d26fa4a9d404990ef3c604306c9b569cc3e50019
santiago alarcon <santiagoalarcon2@hotmail.com>
2019-12-03 20:41:02 -0300

    parte 3

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const app = express();
-
-let enemy = {
-    "name": "orc",
-    "health": 100,
-    "attack": 25
-}
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello Unity Developers!');
-})
+});
 
-app.get('/enemy/orc', (req, res) => {
-    res.send(enemy);
+let enemies = [
+    {
+        "id": 0,
+        "name": "orc",
+        "health": 100,
+        "attack": 25
+    },
+    {
+        "id": 1,
+        "name": "wolf",
+        "health": 110,
+        "attack": 25
+    }
+];
+
+app.get('/enemy', (req, res) => {
+    res.send(enemies);
+});
+
+app.post('/enemy/create', (req, res) => {
+let newEnemy = {
+    "id": req.body.id,
+    "name": req.body.name,
+    "health": req.body.health,
+    "attack": req.body.attack
+};
+
+enemies.push(newEnemy);
+console.log(enemies);
+res.send(enemies);
 });
 
 app.listen(3000, () => console.log('started and listening.'));
-
